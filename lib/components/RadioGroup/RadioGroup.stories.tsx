4c0ea3c0055acf5a07a2ccb757b9e659100d3c2c
MigMarques <migas12340@gmail.com>
2024-05-10 17:09:27 +0100

    fix: fix radio group

---
+++ lib/components/RadioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Label } from "../Label"
+import { RadioGroup, RadioGroupItem } from "./"
+
+/**
+ * ## Installation
+ *
+ * ```sh
+ * $ yarn add @shadcn-ui-sass/radio-group
+ * # or
+ * $ npm install @shadcn-ui-sass/radio-group
+ * ```
+ *
+ */
+
+const meta: Meta<typeof RadioGroup> = {
+  title: "Components/Radio Group",
+  component: RadioGroup,
+  tags: ["autodocs"],
+  args: {
+    disabled: false,
+    defaultValue: "2",
+  },
+}
+
+type Story = StoryObj<typeof RadioGroup>
+
+const template = (args: any) => (
+  <RadioGroup {...args}>
+    <div className="display-flex align-items-center">
+      <RadioGroupItem className="margin-right-xs" value="1" id="1" />
+      <Label htmlFor="1">Option 1</Label>
+    </div>
+    <div className="display-flex align-items-center">
+      <RadioGroupItem className="margin-right-xs" value="2" id="2" />
+      <Label htmlFor="2">Option 2</Label>
+    </div>
+    <div className="display-flex align-items-center">
+      <RadioGroupItem className="margin-right-xs" value="3" id="3" />
+      <Label htmlFor="3">Option 3</Label>
+    </div>
+    <div className="display-flex align-items-center">
+      <RadioGroupItem className="margin-right-xs" value="4" id="4" />
+      <Label htmlFor="4">Option 4</Label>
+    </div>
+  </RadioGroup>
+)
+
+export const Default: Story = {
+  render: (args) => template(args),
+}
+
+export default meta
