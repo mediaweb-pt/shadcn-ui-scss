478f98cb25ffb34b42aeac83c301f6cc5acf17f7
MigMarques <migas12340@gmail.com>
2024-05-10 16:45:48 +0100

    fix: fix badge

---
+++ lib/components/Badge/Badge.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Badge, badgeVariants } from "./"
+
+/**
+ * ## Installation
+ *
+ * ```sh
+ * $ yarn add @shadcn-ui-sass/badge
+ * # or
+ * $ npm install @shadcn-ui-sass/badge
+ * ```
+ *
+ */
+
+const meta: Meta<typeof Badge> = {
+  title: "Components/Badge",
+  component: Badge,
+  tags: ["autodocs"],
+}
+
+type Story = StoryObj<typeof Badge>
+
+const template = (args: any) => <Badge {...args}>Badge</Badge>
+
+export const Default: Story = {
+  render: (args) => template(args),
+}
+
+export const Link: Story = {
+  render: (args) => (
+    <a href="!#" className={badgeVariants({ variant: "outline" })} {...args}>
+      Badge
+    </a>
+  ),
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+  },
+  render: (args) => template(args),
+}
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+  },
+  render: (args) => template(args),
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+  },
+  render: (args) => template(args),
+}
+
+export default meta
