9645e1aec3c4adfcb7da2c2d36832307704729ff
MigMarques <migas12340@gmail.com>
2024-05-10 17:14:20 +0100

    fix: fix skeleton

---
+++ lib/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Skeleton } from "./"
+
+/**
+ * ## Installation
+ *
+ * ```sh
+ * $ yarn add @shadcn-ui-sass/skeleton
+ * # or
+ * $ npm install @shadcn-ui-sass/skeleton
+ * ```
+ *
+ */
+
+const meta: Meta<typeof Skeleton> = {
+  title: "Components/Skeleton",
+  component: Skeleton,
+  tags: ["autodocs"],
+}
+
+type Story = StoryObj<typeof Skeleton>
+
+const template = (args: any) => (
+  <div>
+    <Skeleton
+      {...args}
+      style={{ width: "600px", height: "24px", marginBottom: "16px" }}
+    />
+    <Skeleton {...args} style={{ width: "500px", height: "48px" }} />
+  </div>
+)
+
+export const Default: Story = {
+  render: (args) => template(args),
+}
+
+export default meta
