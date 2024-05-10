9d78ed6a0843d4cdef74dd722433e5bbe8a9b0e9
MigMarques <migas12340@gmail.com>
2024-05-10 16:48:10 +0100

    fix: fix breadcrumbs

--- lib/components/Breadcrumbs/Breadcrumbs.tsx
+++ lib/components/Breadcrumbs/Breadcrumbs.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { ChevronRight, Ellipsis } from "lucide-react"
 
-import { cn } from "../../../utils/utils"
+import { cn } from "../../utils/utils"
 
 //import "./Breadcrumbs.scss"
 
